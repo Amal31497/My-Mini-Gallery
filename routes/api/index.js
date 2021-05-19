95dc922f77f79688b686e026c7876a44caa2d1c6
Amal Janabayev <amalj2426@gmail.com>
2021-05-18 21:27:27 -0700

    Wrote couple controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const artRoutes = require("./art");
-const genreRoutes = require("./genre");
-const userRoutes = require("./user");
-const commentsRoutes = require("./comment")
+// const genreRoutes = require("./genre");
+// const userRoutes = require("./user");
+// const commentsRoutes = require("./comment")
 
-// Post routes
 router.use("/art", artRoutes);
-router.use("/genre", genreRoutes);
-router.use("/user", userRoutes);
-router.use("/comment", commentsRoutes);
+// router.use("/genre", genreRoutes);
+// router.use("/user", userRoutes);
+// router.use("/comment", commentsRoutes);
 
 module.exports = router;
