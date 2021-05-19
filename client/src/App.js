f67638fe6ee3c0cc7bdd658f8971aad067bf16d2
Hanh Le <hanhdtle@gmail.com>
2021-05-18 20:41:04 -0700

    initial react front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header"
+import Particles from 'react-particles-js';
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 1000
+              }
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                encable: true,
+                speed: 0.2,
+                opacity_min: 0,
+                sync: false
+              }
+            },
+            size: {
+              value: 10,
+              random: true,
+              anim: {
+                encable: true,
+                speed: 2,
+                opacity_min: 5,
+                sync: false
+              }
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: .5,
+              direction: "top",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+              }
+            }
+
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+      <Footer />
+    </>
   );
 }
 
